5145040ce8fc06d9e423918f1d1cd5c8966e474e
Neo-AzTeK <dimitri.faj@gmail.com>
2025-10-16 18:51:06 +0200

    feat: add checks to scripts + add copy docker-compose.yml to root project directoy

--- deployment-prod.sh
+++ deployment-prod.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+if ! command -v git &> /dev/null; then
+    echo "Git is not installed. Please install it first."
+    exit 1
+fi
+
+if ! command -v docker &> /dev/null; then
+    echo "Docker is not installed. Please install Docker and Docker Compose."
+    exit 1
+fi
+
 mkdir CodeClique
 cd CodeClique
 
@@ -7,5 +17,10 @@ git clone https://github.com/CodeClique-RobertDoisneau/backend.git
 git clone https://github.com/CodeClique-RobertDoisneau/frontend.git
 git clone https://github.com/CodeClique-RobertDoisneau/deployment.git
 cd deployment
+
+if [ ! -f "docker-compose.prod.yml" ]; then
+    echo "docker-compose.prod.yml not found!"
+    exit 1
+fi
 
 docker compose -f docker-compose.prod.yml up -d
